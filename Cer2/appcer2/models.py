16dc291fda3438bf7964899ae3de9cf60902a616
JOlave <jaime.olave@usm.cl>
2022-11-13 03:00:14 -0300

    fixed

--- Cer2/appcer2/models.py
+++ Cer2/appcer2/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Residencia(models.Model):
-    numero  = models.CharField(max_length=50, unique=True)
+    numero  = models.IntegerField( primary_key=True)
     dueño = models.CharField(max_length=50)
     telefono = models.IntegerField()
     Correo = models.CharField(max_length=50)
     def __str__(self):
-        return self.numero
+        return "Residencia "+str(self.numero)
 
 
 class Correspondencia(models.Model):
@@ -16,4 +16,4 @@ class Correspondencia(models.Model):
     destinatario = models.CharField(max_length=90)
     direccion = models.ForeignKey(Residencia, on_delete=models.CASCADE)
     def __int__(self):
-        return self.direccion
+        return str(self.direccion)
